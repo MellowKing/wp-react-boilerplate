ceda885fa01b5ba662dbbb00177ce2ac1e35a81e
MellowKing <marten.truu@gmail.com>
2016-04-14 15:48:49 +0300

    webpack can has build nao

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const PATHS = {
+  src: path.join(__dirname, 'src'),
+  build: path.join(__dirname, 'build'),
+}
+
+module.exports = {
+  entry: [
+    'webpack-hot-middleware/client',
+    path.join(PATHS.src, '/index.js'),
+  ],
+  output: {
+    path: PATHS.build,
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.join(PATHS.src, '/index.ejs'),
+    }),
+  ]
+}
